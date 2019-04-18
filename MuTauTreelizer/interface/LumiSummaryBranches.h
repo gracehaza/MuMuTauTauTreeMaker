5a6783b530be1911c27ffb54761fb1d4cc7a433c
Fengwangdong <fengwangdong.zhang@cern.ch>
2019-04-18 12:13:12 +0200

    add Gen weight flat tree for MCs

---
+++ MuTauTreelizer/interface/LumiSummaryBranches.h
@@ -0,0 +1,29 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/LuminosityBlock.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+
+#include "TTree.h"
+
+class LumiSummaryBranches {
+  public:
+    LumiSummaryBranches(TTree * tree, const edm::ParameterSet& iConfig, edm::ConsumesCollector cc);
+    void beginLumi(const edm::LuminosityBlock& iEvent);
+    void fill(const edm::Event& iEvent);
+
+  private:
+    edm::EDGetTokenT<GenEventInfoProduct> genEventInfoToken_;
+    edm::EDGetTokenT<int> neventsToken_;
+    edm::EDGetTokenT<float> summedWeightsToken_;
+    bool hasSummary_;
+    // branches
+    Int_t   runBranch_;
+    Int_t   lumiBranch_;
+    Int_t   neventsBranch_;
+    Float_t summedWeightsBranch_;
+
+};
